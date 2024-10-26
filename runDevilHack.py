5e156c2fffc3f23284bcf2e1cab604b21de1313d
No_Name.exe <153848626+NoNameZusy@users.noreply.github.com>
2024-10-26 17:32:18 +0400

    Create runDevilHack.py

---
+++ runDevilHack.py
@@ -0,0 +1,14 @@
+import os 
+import time
+
+print("KALI LINUXUNU SIKIYOZ EZZZ")
+time.sleep(0.1)
+print("KALI LINUXUNU SIKIYOZ EZZZ")
+time.sleep(0.1)
+print("KALI LINUXUNU SIKIYOZ EZZZ")
+time.sleep(0.1)
+print("KALI LINUXUNU SIKIYOZ EZZZ")
+time.sleep(0.1)
+print("KALI LINUXUNU SIKIYOZ EZZZ")
+time.sleep(0.1)
+os.system('sudo systemctl restart lightdm")
